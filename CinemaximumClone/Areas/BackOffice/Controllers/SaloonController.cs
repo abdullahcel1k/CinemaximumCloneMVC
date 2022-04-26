db2a24a80a9f64d2f39289a4289450eb5630e04c
Abdullah Çelik <abdullahcelkk@gmail.com>
2022-04-26 19:50:01 +0300

    add saloon controller

---
+++ CinemaximumClone/Areas/BackOffice/Controllers/SaloonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaximumClone.Areas.BackOffice.Controllers
+{
+    [Area("BackOffice")]
+    public class SaloonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
